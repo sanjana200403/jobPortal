fc41d63b2bcbc6a7d6070e29bcbd05b93a775414
sanjana <03sanjana2004@gmail.com>
2024-04-26 17:27:43 +0530

    changes

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,8 +2,7 @@ export const sendToken = (user,statusCode,res,message)=>{
 const token = user.getJWTToken();
 const options={
     expires:new Date(Date.now()+process.env.COOKIE_EXPIRE *24*60*60*1000),
-    httpOnly:true,
-    secure:true
+    httpOnly: true, sameSite: "none", secure: true 
 
 }
 res.status(statusCode).cookie("token",token,options).json({
